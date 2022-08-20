0846165a6461906598ee5215185023870757b864
AshNlandofOzz <ashleyosburn88@gmail.com>
2022-08-20 13:29:48 -0600

    first submission for credit of project. Please see README for thoughts.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # MakeChangeProject
+This project is complete for my first attempt.
+
+#Bugs
+While this project does meet all the qualifications, there is a bug that I have encountered difficulty with. The cents that are published by my equation are not two decimals. They are often much longer. This creates a pub with the denominations.
+
+#Reasons for the Bug
+I am sure this is being caused as the information is passed from a Double to and Int so I can isolate the denominations. That being said, I am unsure as of right now how to fix this bug, but plan on investigating further through the weekend to try to find a solution, though I am unsure at my level of knowledge that I will find a better solution by the assignment due date so I am pushing my current work to ensure I meet deadline.
+
+Thank you.
--- src/SkillDistilleryProject/MakeChange.java
+++ src/SkillDistilleryProject/MakeChange.java
@@ -1,10 +1,62 @@
 package SkillDistilleryProject;
 
+import java.util.Scanner;
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner (System.in);
+		
+		System.out.println("Enter the purchase amount: ");
+		double price = scan.nextDouble();
+		System.out.println("Enter the amount paid: ");
+		double amount= scan.nextDouble();
+		System.out.println("Total change = " + (amount - price));
+		changeCalc(price, amount);	
+				
+		
+		scan.close();
 	}
+	public static void changeCalc(double cost, double paid) {
 
-}
+		double change = paid - cost;
+			if (change >= 20) {
+					 System.out.println((int)(change/20) + ": Twenty" );
+					 change = change % 20;
+				 }
+			 if (change >= 10) {
+				 System.out.println((int)(change/10) + ": Ten" );
+				 change = change % 10;
+			 } 
+			 if (change >= 5) {
+				 System.out.println((int)(change/5) + ": Five" );
+				 change = change % 5;
+			 }  
+			 if (change >= 1) {
+				 System.out.println((int)(change/1) + ": One" );
+				 change = change % 1;
+			 }  
+			 if (change >= .25) {
+				 System.out.println((int)(change/.25) + ": Quarter" );
+				 change = change % .25;
+			 }  
+			 if (change >= .10) {
+				 System.out.println((int)(change/.10) + ": Dime" );
+				 change = change % .10;
+			 }  
+			 if (change >= .05) {
+				 System.out.println((int)(change/.05) + ": Nickel" );
+				 change = change % .05;
+			 }  
+			 if (change >= .01) {
+				 System.out.println((int)(change/.01) + ": Penny" );
+				 change = change % .05;
+			 }  
+			 else if (change == 0) {
+				 System.out.println("No change due. ");
+			 }
+			 else {
+				 System.out.println("System error. Not enough money tendered.");
+			 }
+
+		}
+	}
